8ba6ef9373282f27a476a42cb7fb6cdaa453ee85
Memed555 <mehemmedelibeyli555@gmail.com>
2024-11-11 00:26:05 +0400

    Configurate Backend to deploy on vercel

--- Backend/config/connectDb.js
+++ Backend/config/connectDb.js
@@ -1,20 +1,3 @@
-// require('dotenv').config()
-// const { Sequelize } = require('sequelize')
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: 'postgres',
-//   timezone: '+04:00',
-//   logging:false
-// })
-// sequelize.authenticate()
-//   .then(() => { console.log('Database succesfully connected') })
-//   .catch((error) => { console.log('Connection error :', error) })
-
-
-// module.exports = sequelize
-
 
 
 const { Sequelize } = require("sequelize");
@@ -28,6 +11,7 @@ const sequelize = new Sequelize(
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT || 5432, // Default to 5432 if no port is provided
     dialect: "postgres",
+    dialectModule: require('pg'),
     logging: false,
     dialectOptions: {
       ssl: {
