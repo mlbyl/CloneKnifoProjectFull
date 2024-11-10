1f20cc9768d1d5a496a77082afd0f5a9f48c2e58
Memed555 <mehemmedelibeyli555@gmail.com>
2024-11-11 00:20:52 +0400

    Added vercel speed insights to Frontend main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,12 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <ToastContainer />
+    <SpeedInsights />
   </StrictMode>
 );
